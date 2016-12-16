608e51db64c6587abad22c869f9a651adc341803
Lucodivo <ConnorAHaskins@gmail.com>
2016-12-16 10:35:44 -0800

    Add project files.

---
+++ Maze/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                // run code
+            }
+            else
+            {
+                Console.WriteLine("Wrong number of arguments.");
+                Console.WriteLine("Format: ");
+            }
+        }
+    }
+}
